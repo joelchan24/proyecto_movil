821d0e2f69f14ffcdbf06220698ca7c6eb7ca139
Adan98 <31905117+Adan98@users.noreply.github.com>
2018-02-21 19:05:30 -0600

    Registro Usuarios

--- app/src/main/java/com/example/joel/proyecto/MainActivity.java
+++ app/src/main/java/com/example/joel/proyecto/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.joel.proyecto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button botonregistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
